a19e321073839a911887c6be9d1780565aa450f8
LucaRiccio <lucariccio888@gmail.com>
2020-07-14 16:12:21 +0200

    Esercizio terminato

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert("prova");
+for (var i = 1; i <= 100; i++) {
+
+  if (i % 5 == 0 && i % 3 == 0) {
+    console.log("Buzz" + "Fizz");
+  }
+
+  else if (i % 3 == 0) {
+    console.log("Fizz");
+  }
+
+  else if (i % 5 == 0) {
+    console.log("Buzz");
+  }
+
+  else {
+    console.log(i);
+  }
+
+}
